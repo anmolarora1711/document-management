87e029ee8ee82deb411280fd4f124dec9d107d81
Anmol Arora <anmolkdeneon@gmail.com>
2025-01-16 11:51:17 +0530

    user controllers added

---
+++ src/user/user.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Body, Post, Delete, UseGuards, Query, Param } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { UserService } from './user.service';
+import { Roles } from '../common/decorators/roles.decorator';
+
+@Controller('users')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Get()
+    @Roles('admin')
+    async allUsers(@Query('roleId') roleId?: string) {
+        if (roleId) {
+            if (isNaN(parseInt(roleId))) {
+                throw new Error('role id should be number');
+            }
+            return this.userService.findUsersByRole(parseInt(roleId));
+        }
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    @Roles('admin')
+    async getUserById(@Param('id') id: string) {
+        if (isNaN(parseInt(id))) {
+            throw new Error('user id should be number');
+        }
+        return this.userService.findById(parseInt(id));
+    }
+
+    @Delete(':id')
+    @Roles('admin')
+    async deleteUser(@Param('id') id: number) {
+        return this.userService.deleteUser(id);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -24,4 +24,27 @@ export class UserService {
     async save(user: User): Promise<User> {
         return this.userRepository.save(user);
     }
+
+    // Find all the users
+    async findAll() {
+        return this.userRepository.find();
+    }
+
+    // Find users by role id
+    async findUsersByRole(roleId: number) {
+        return this.userRepository.find({ where: { role: { id: roleId} } });
+    }
+
+    async findById(userId: number) {
+        return this.userRepository.findOne({ where: { id: userId } });
+    }
+
+    async deleteUser(userId: number) {
+        const user = await this.userRepository.findOne({ where: { id: userId } });
+        if (!user) {
+            throw new Error('User not found');
+        }
+        await this.userRepository.remove(user);
+        return { message: 'User deleted successfully' };
+    }
 }
