d86cb558950b87d136aa0e2934e534207d568f64
Anmol Arora <anmolkdeneon@gmail.com>
2025-01-20 22:41:04 +0530

    health check added

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthCheck(): any {
+    return { status: 'UP', timestamp: new Date() };
   }
 }
